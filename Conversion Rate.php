4cee46d959da6357b361f9aa025629a796bb89ba
tahsinRudro <118099591+tahsinRudro@users.noreply.github.com>
2022-11-13 13:51:25 +0600

    Add files via upload

---
+++ Conversion Rate.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Conversion</title>
+</head>
+<body>
+        <center>
+        <h1>Page2[Conversion]</h1><br></br>
+</center>
+<h3>Conversion Site</h3><br>
+1.<a href="Home.php"> <b>  Home </b></a> 
+2.<a href="Conversion Rate.php"> <b>  Conversion Rate </b></a> 
+3.<a href="History.php"><b> History </b></a><br>
+<h3>Conversion Rate :</h3><br>
+<label for="fname">Category:</label>
+<input type="text" id="fname" name="fname"><br></br>
+<label for="quantity">Unit:</label>
+<input type="number" id="quantity"><br></br>
+<label for="quantity">Rate:</label>
+<input type="number" id="quantity">
+<input type="submit" name="submit" value="Submit"><br></br>
+		
+			
+			
+
+
+        
+        
+</html>
---
+++ Home.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Home</title>
+</head>
+<body>
+        <center>
+        <h1>Page1[Home]</h1><br></br>
+</center>
+<h3>Conversion Site</h3><br>
+2.<a href="Conversion Rate.php"> <b>  Conversion Rate </b></a> 
+3.<a href="History.php"><b> History </b></a><br>
+<h3>Converter :</h3><br>
+Select Catagory: <select name="Catagory" id="Catagory">
+			
+			<option value="Usd to Bdt">Usd to Bdt</option>
+			<option value="Bdt to Usd">Bdt to Usd</option></br>
+                        <br></br>
+                       
+			<label for="quantity">Value :</label>
+<input type="number" id="quantity" name="quantity" >
+		<input type="submit" name="submit" value="Submit"><br></br>
+                <label for="phone">Value:</label>
+<input type="tel" id="phone" name="phone" >
+		
+			
+			
+
+
+        
+        
+</html>
